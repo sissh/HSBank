8428b45cab87a6a22eebaefe1cd8146cad4f4b24
sissh <sissani.hani@gmail.com>
2023-01-11 19:53:12 +0100

    Exercice 4 - Implement Reading XML & JSON - FINAL COMMIT

--- HSBank/src/main/Main.java
+++ HSBank/src/main/Main.java
@@ -1,7 +1,14 @@
 package main;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 import components.*;
 import utils.*;
@@ -36,7 +43,7 @@ public class Main {
 		flows.add(new Transfer(1, "Transfer from account 1 to account 2", "dazazadzah", 60.00, 2, true, LocalDate.now().plusDays(2)));
 		
 		//1.3.5 Updating accounts
-		System.out.println("Accounts before transactions : \n");
+		/*System.out.println("Accounts before transactions : \n");
 		Display.displayHashAccounts(hashAccounts);
 		System.out.println("\n");
 		System.out.println("Accounts After transactions : \n");
@@ -46,12 +53,31 @@ public class Main {
 		//1.3.5 Updating accounts - Check for negative accounts
 		System.out.println("\n");
 		String negative = Operation.checkNegativeAccounts(hashAccounts);
-		System.out.println(negative);
+		System.out.println(negative);*/
 		
-		//2.1 JSON file of flows
+		//2.1 JSON file of flows - Write
 		Operation.writeFlowsToJson(flows);
 		
-		//2.2 XML file of accounts
+		//2.2 XML file of accounts - Write
 		Operation.writeAccountsToXML(hashAccounts);
+		
+		//2.1 JSON file of flows - Read
+		Path pathJson = FileSystems.getDefault().getPath("resources", "flows.json");
+		try {
+			ArrayList<Flow> flowsFromJson= Operation.readFlowsFromJSON(pathJson);
+			flowsFromJson.stream().forEach(System.out::println);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//2.2  XML file of accounts - Read
+		Path pathXML = FileSystems.getDefault().getPath("resources", "accounts.xml");
+		try {
+			ArrayList<Account> accountsFromXML = Operation.readAccountsFromXML(pathXML);
+			accountsFromXML.stream().forEach(System.out::println);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
--- HSBank/src/utils/Operation.java
+++ HSBank/src/utils/Operation.java
@@ -1,13 +1,25 @@
 package utils;
 import java.util.ArrayList;
-
 import java.util.HashMap;
 import java.util.Random;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+
+import org.json.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import components.*;
 
@@ -73,17 +85,18 @@ public class Operation {
 		return message;
 	}
 	
-	//2.1 JSON file of flows
+	//2.1 JSON file of flows - writing
 	public static void writeFlowsToJson(ArrayList<Flow> flows) {
 		Path path = FileSystems.getDefault().getPath("resources", "flows.json");
-		String output = "{\n\"flows\": [ \n";
+		String output = "[\n";
 		for (int i = 0; i < flows.size(); i++) {
 			if (flows.get(i) instanceof Transfer transfer) {
 				output += "  { \n"
+						+ "    \"flowType\": \"Transfer\",\n"
 						+ "    \"issuerAccountNumber\": \"" + transfer.getIssuerAccountNumber() + "\",\n"
 						+ "    \"targetAccountNumber\": \"" + transfer.getTargetAccountNumber() + "\",\n"
-						+ "    \"flowType\": \"Transfer\",\n"
-						+ "    \"amount\": \"" +  String.format("%.2f", transfer.getAmount()) + "\",\n"
+						+ "    \"identifier\": \"" + transfer.getIdentifier() + "\",\n"
+						+ "    \"amount\": \"" +  transfer.getAmount() + "\",\n"
 						+ "    \"comment\": \"" + transfer.getComment() + "\",\n"
 						+ "    \"effect\": \"" + transfer.getEffect().toString() + "\",\n"
 						+ "    \"date\": \"" + transfer.getDate().toString() + "\"\n";
@@ -92,7 +105,8 @@ public class Operation {
 				output += "  { \n"
 						+ "    \"flowType\": \"Debit\",\n"
 						+ "    \"targetAccountNumber\": \"" + debit.getTargetAccountNumber() + "\",\n"
-						+ "    \"amount\": \"" +  String.format("%.2f", debit.getAmount()) + "\",\n"
+						+ "    \"identifier\": \"" + debit.getIdentifier() + "\",\n"
+						+ "    \"amount\": \"" +  debit.getAmount() + "\",\n"
 						+ "    \"comment\": \"" + debit.getComment() + "\",\n"
 						+ "    \"effect\": \"" + debit.getEffect().toString() + "\",\n"
 						+ "    \"date\": \"" + debit.getDate().toString() + "\"\n";
@@ -102,7 +116,8 @@ public class Operation {
 				output += "  { \n"
 						+ "    \"flowType\": \"Credit\",\n"
 						+ "    \"targetAccountNumber\": \"" + credit.getTargetAccountNumber() + "\",\n"
-						+ "    \"amount\": \"" +  String.format("%.2f", credit.getAmount()) + "\",\n"
+						+ "    \"identifier\": \"" + credit.getIdentifier() + "\",\n"
+						+ "    \"amount\": \"" +  credit.getAmount() + "\",\n"
 						+ "    \"comment\": \"" + credit.getComment() + "\",\n"
 						+ "    \"effect\": \"" + credit.getEffect().toString() + "\",\n"
 						+ "    \"date\": \"" + credit.getDate().toString() + "\"\n";
@@ -113,8 +128,8 @@ public class Operation {
 				output += "  }";
 			}
 		}
+		output += "]";
 		
-		output += "\n ]\n}";
 		try {
 			Files.write(path, output.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
 		} catch (IOException e) {
@@ -122,29 +137,34 @@ public class Operation {
 		}
 	}
 	
-	//2.2 XML file of flows
+	//2.2 XML file of accounts - writing
 	public static void writeAccountsToXML(HashMap<Integer, Account> hashAccounts) {
 		Path path = FileSystems.getDefault().getPath("resources", "accounts.xml");
 		ArrayList<Account> accounts = new ArrayList<Account>(hashAccounts.values());
 		
-		String output = "<Accounts>\n";
+		String output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Accounts>\n";
 		
 		for (int i = 0; i < accounts.size(); i++) {
 			if (accounts.get(i) instanceof CurrentAccount currentAccount) {
 				output += "  <CurrentAccount>\n" 
 						+ "    <AccountNumber>" + currentAccount.getAccountNumber() + "</AccountNumber>\n"
-						+ "    <Client>" + currentAccount.getClient().getFirstName() + " " + currentAccount.getClient().getName() + "</Client>\n"
-						+ "    <Label>" + currentAccount.getLabel() + "</Label>\n"
-						+ "    <Balance>" + String.format("%.2f", currentAccount.getBalance()) + "</Balance>\n"
+						+ "    <Client>\n"  
+							+ "      <Name>" + currentAccount.getClient().getName() + "</Name>\n"
+							+ "      <FirstName>" + currentAccount.getClient().getFirstName() + "</FirstName>\n"
+							+ "      <ClientNumber>" + currentAccount.getClient().getClientNumber() + "</ClientNumber>\n" + "    </Client>\n"						+ "    <Label>" + currentAccount.getLabel() + "</Label>\n"
+						+ "    <Balance>" + currentAccount.getBalance() + "</Balance>\n"
 						+ "  </CurrentAccount>\n\n";
 			}
 			
 			if (accounts.get(i) instanceof SavingsAccount savingsAccount) {
 				output += "  <SavingsAccount>\n" 
 						+ "    <AccountNumber>" + savingsAccount.getAccountNumber() + "</AccountNumber>\n"
-						+ "    <Client>" + savingsAccount.getClient().getFirstName() + " " + savingsAccount.getClient().getName() + "</Client>\n"
+						+ "    <Client>\n"  
+							+ "      <Name>" + savingsAccount.getClient().getName() + "</Name>\n"
+							+ "      <FirstName>" + savingsAccount.getClient().getFirstName() + "</FirstName>\n"
+							+ "      <ClientNumber>" + savingsAccount.getClient().getClientNumber() + "</ClientNumber>\n" + "    </Client>\n"
 						+ "    <Label>" + savingsAccount.getLabel() + "</Label>\n"
-						+ "    <Balance>" + String.format("%.2f", savingsAccount.getBalance()) + "</Balance>\n"
+						+ "    <Balance>" + savingsAccount.getBalance() + "</Balance>\n"
 						+ "  </SavingsAccount>\n\n";
 			}
 		}
@@ -156,5 +176,123 @@ public class Operation {
 		}
 	}
 	
+	//2.1 JSON file of flows - reading
+	public static ArrayList<Flow> readFlowsFromJSON(Path path) throws IOException {
+		ArrayList<Flow> flows = new ArrayList<>();
+		String fileOutput = new String(Files.readAllBytes(path));
+		JSONArray json = new JSONArray(fileOutput);
+		
+		for (int i = 0; i < json.length(); i++) {
+			if (json.get(i) instanceof JSONObject jsonObj) {
+				if (jsonObj.get("flowType").equals("Transfer")) {
+					Integer issuerAccountNumber = Integer.parseInt(jsonObj.get("issuerAccountNumber").toString());
+					Integer targetAccountNumber = Integer.parseInt(jsonObj.get("targetAccountNumber").toString());
+					String identifier = jsonObj.get("identifier").toString();
+					Double amount = Double.parseDouble(jsonObj.get("amount").toString());
+					String comment = jsonObj.get("comment").toString();
+					Boolean effect = Boolean.parseBoolean(jsonObj.get("effect").toString());
+					LocalDate date = LocalDate.parse(jsonObj.get("date").toString());
+					flows.add(new Transfer(issuerAccountNumber, comment, identifier, amount, targetAccountNumber, effect, date));
+				}
+				
+				if (jsonObj.get("flowType").equals("Credit")) {
+					Integer targetAccountNumber = Integer.parseInt(jsonObj.get("targetAccountNumber").toString());
+					String identifier = jsonObj.get("identifier").toString();
+					Double amount = Double.parseDouble(jsonObj.get("amount").toString());
+					String comment = jsonObj.get("comment").toString();
+					Boolean effect = Boolean.parseBoolean(jsonObj.get("effect").toString());
+					LocalDate date = LocalDate.parse(jsonObj.get("date").toString());
+					flows.add(new Credit(comment, identifier, amount, targetAccountNumber, effect, date));
+				}
+				
+				if (jsonObj.get("flowType").equals("Debit")) {
+					Integer targetAccountNumber = Integer.parseInt(jsonObj.get("targetAccountNumber").toString());
+					String identifier = jsonObj.get("identifier").toString();
+					Double amount = Double.parseDouble(jsonObj.get("amount").toString());
+					String comment = jsonObj.get("comment").toString();
+					Boolean effect = Boolean.parseBoolean(jsonObj.get("effect").toString());
+					LocalDate date = LocalDate.parse(jsonObj.get("date").toString());
+					flows.add(new Debit(comment, identifier, amount, targetAccountNumber, effect, date));
+				}
+			}
+		}
+		return flows;
+	}
 	
+	//2.2 XML file of accounts - reading
+	public static ArrayList<Account> readAccountsFromXML(Path path) throws ParserConfigurationException, SAXException, IOException {
+		ArrayList<Account> accounts = new ArrayList<>();
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document doc = builder.parse(Files.newInputStream(path));
+		
+		String name = "";
+		String firstName = "";
+		Integer clientNumber = 0;
+		Integer accountNumber = 0;
+		String label = "";
+		Double balance = 0.00;
+		
+		NodeList currentAccountNodes = doc.getElementsByTagName("CurrentAccount");
+		for (int k = 0; k < currentAccountNodes.getLength(); k++) {
+			Node currentAccountNode = currentAccountNodes.item(k);
+			if (currentAccountNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element currentAccount = (Element) currentAccountNode;
+				accountNumber = Integer.parseInt(currentAccount.getElementsByTagName("AccountNumber").item(0).getTextContent());
+				label = currentAccount.getElementsByTagName("Label").item(0).getTextContent();
+				balance = Double.parseDouble(currentAccount.getElementsByTagName("Balance").item(0).getTextContent());
+				
+				NodeList clientAttributesNodes = currentAccount.getElementsByTagName("Client");
+				for (int l = 0; l < clientAttributesNodes.getLength(); l++) {
+					Node clientAttributesNode = clientAttributesNodes.item(l);
+					if (clientAttributesNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element clientAttributes = (Element) clientAttributesNode;
+						name = clientAttributes.getElementsByTagName("Name").item(0).getTextContent();
+						firstName = clientAttributes.getElementsByTagName("FirstName").item(0).getTextContent();
+						clientNumber = Integer.parseInt(clientAttributes.getElementsByTagName("ClientNumber").item(0).getTextContent());
+					}
+				}
+				
+				Client client = new Client(name, firstName);
+				client.setClientNumber(clientNumber);
+				
+				CurrentAccount newcurrentAccount = new CurrentAccount(label, client);
+				newcurrentAccount.setBalance(balance);
+				newcurrentAccount.setAccountNumber(accountNumber);
+				accounts.add(newcurrentAccount);
+			}
+		}
+			
+		NodeList savingsAccountNodes = doc.getElementsByTagName("SavingsAccount");
+		for (int k = 0; k < savingsAccountNodes.getLength(); k++) {
+			Node savingsAccountNode = savingsAccountNodes.item(k);
+			if (savingsAccountNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element savingsAccount = (Element) savingsAccountNode;
+				accountNumber = Integer.parseInt(savingsAccount.getElementsByTagName("AccountNumber").item(0).getTextContent());
+				label = savingsAccount.getElementsByTagName("Label").item(0).getTextContent();
+				balance = Double.parseDouble(savingsAccount.getElementsByTagName("Balance").item(0).getTextContent());
+				
+				NodeList clientAttributesNodes = savingsAccount.getElementsByTagName("Client");
+				for (int l = 0; l < clientAttributesNodes.getLength(); l++) {
+					Node clientAttributesNode = clientAttributesNodes.item(l);
+					if (clientAttributesNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element clientAttributes = (Element) clientAttributesNode;
+						name = clientAttributes.getElementsByTagName("Name").item(0).getTextContent();
+						firstName = clientAttributes.getElementsByTagName("FirstName").item(0).getTextContent();
+						clientNumber = Integer.parseInt(clientAttributes.getElementsByTagName("ClientNumber").item(0).getTextContent());
+					}
+				}
+				
+				Client client = new Client(name, firstName);
+				client.setClientNumber(clientNumber);
+				
+				SavingsAccount newSavingsAccount = new SavingsAccount(label, client);
+				newSavingsAccount.setBalance(balance);
+				newSavingsAccount.setAccountNumber(accountNumber);
+				accounts.add(newSavingsAccount);
+			}
+		}
+		
+		return accounts;
+	}
 }
